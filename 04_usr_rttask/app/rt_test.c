8067e6acc8fcec892f8ea571ddc60480719e795d
Will8.Chen <Will8.Chen@advantech.com.tw>
2024-06-26 17:19:48 +0800

    update example

---
+++ 04_usr_rttask/app/rt_test.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+//#include <sys/io.h>
+#include <stdlib.h>  
+#include <string.h> 
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <dirent.h>
+#include <ctype.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#include <alchemy/task.h>
+#include <alchemy/timer.h>
+#include <trank/native/timer.h>
+#include <trank/native/task.h>
+#include <rtdm/rtdm.h>
+
+
+#define TASK_PRIO 90 // 99 is Highest RT priority, 0 is Lowest
+#define TASK_MODE 0 // No flags
+#define TASK_STKSZ 0 // default Stack size
+
+#define TASK_PERIOD (10*1000*1000) // 0.5= 50,000,000 ns
+
+
+
+#define DEV_BASIC0	"rtdm0"
+#define DEV_BASIC1	"rtdm1"
+
+
+#define RTTST_RTDM_DEFER_CLOSE_CONTEXT   1
+#define RTTST_RTDM_MAGIC_PRIMARY        0xfefbfefb
+#define RTTST_RTDM_MAGIC_SECONDARY      0xa5b9a5b9
+
+#define RTDM_CLASS_TESTING          6
+#define RTIOC_TYPE_TESTING              RTDM_CLASS_TESTING
+#define RTTST_RTIOC_RTDM_DEFER_CLOSE \
+        _IOW(RTIOC_TYPE_TESTING, 0x40, __u32)
+
+#define RTTST_RTIOC_RTDM_ACTOR_GET_CPU \
+        _IOR(RTIOC_TYPE_TESTING, 0x41, __u32)
+
+#define RTTST_RTIOC_RTDM_PING_PRIMARY \
+        _IOR(RTIOC_TYPE_TESTING, 0x42, __u32)
+
+#define RTTST_RTIOC_RTDM_PING_SECONDARY \
+        _IOR(RTIOC_TYPE_TESTING, 0x43, __u32)
+
+
+
+static int dev[2], ret[2];
+
+void periodic_task (void *arg) {
+    
+    int cnt=0, err, from_drv;
+    long diff=0;
+    RTIME now, previous;
+    previous= rt_timer_read();
+    printf("before XENO loop:\n");
+    
+    if(rt_task_set_periodic(NULL, TM_NOW, rt_timer_ns2ticks(TASK_PERIOD)))
+        printf("rt_task_set_periodic failed \n");
+    
+    //printf("Press Any key to start rt task\n");
+    //getchar();
+
+    /* open the device */
+    dev[0] = rt_dev_open(DEV_BASIC0, 0);
+    dev[1] = rt_dev_open(DEV_BASIC1, 0);
+    
+    if (dev[0] < 0 || dev[1] < 0) {
+        printf("ERROR : can't open device %s (%s) or device %s (%s)\n",
+               DEV_BASIC0, strerror(-dev[0]), 
+               DEV_BASIC1, strerror(-dev[1]));
+        fflush(stdout);
+        return;
+    }
+    
+    rt_printf("open sucess %d %d \n", dev[0], dev[1]);
+
+    for (;;) {
+        
+        err = rt_dev_ioctl (dev[0], RTTST_RTIOC_RTDM_PING_PRIMARY, &from_drv);
+        rt_printf("from_drv 0x%0x \n", from_drv);
+        //err = rt_dev_ioctl (dev[0], RTTST_RTIOC_RTDM_PING_SECONDARY, &from_drv);
+        //rt_printf("from_drv 0x%0x \n", from_drv);
+        //err = rt_dev_ioctl (dev[0], RTTST_RTIOC_RTDM_DEFER_CLOSE, RTTST_RTDM_DEFER_CLOSE_CONTEXT);
+        
+        //task migrates to primary mode with xeno API call
+        rt_task_wait_period(NULL); //deschedule until next period.
+	//usleep(TASK_PERIOD/1000);
+        now = rt_timer_read(); //cureent time
+    
+        //task migrates to secondary mode with syscall
+        //so printf may have unexpected impact on the timing
+        diff = now - previous;
+        
+        if(diff > 1.5*TASK_PERIOD)
+	        rt_printf("Time elapsed: %ld.%04ld ms\n",
+                    (long)diff / 1000000,
+                    (long)diff % 1000000);
+        
+        previous = now;
+    }
+}
+
+int main (int argc, char *argv[])
+{
+    int e1=0, e2, e3=0, e4;
+    RT_TIMER_INFO info;
+    RT_TASK tA;
+    
+    mlockall(MCL_CURRENT|MCL_FUTURE);
+    
+
+    //e1 = rt_timer_set_mode(TM_ONESHOT); // Set oneshot timer
+    e2 = rt_task_create(&tA, "periodicTask", TASK_STKSZ, TASK_PRIO, TASK_MODE);
+    
+    //set period
+    //e3 = rt_task_set_periodic(&tA, TM_NOW, rt_timer_ns2ticks(TASK_PERIOD));
+    e4 = rt_task_start(&tA, &periodic_task, NULL); 
+    
+    if (e1 | e2 | e3 | e4) {
+        fprintf(stderr, "Error launching periodic task....\n");
+        rt_task_delete(&tA);
+        exit(1);
+    }
+
+    printf("Press any key to end....\n");
+    getchar();
+    rt_task_delete(&tA);
+    
+    /* close the device */
+    ret[0] = rt_dev_close(dev[0]);
+    ret[1] = rt_dev_close(dev[1]);
+    if (ret[0] < 0 || ret[1] < 0) {
+        printf("ERROR : can't open device %s (%s) or device %s (%s)\n",
+               DEV_BASIC0, strerror(-ret[0]), 
+               DEV_BASIC1, strerror(-ret[1]));
+        fflush(stdout);
+        return;
+    }
+
+    return 0;
+}
---
+++ 05_usr_rtdm_ioctl/app/mydrivertest.c
@@ -0,0 +1,148 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by trem (Philippe Reynes)                          *
+ *   tremyfr@yahoo.fr                                                      *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+/**
+ * This is an example that shows how RTDM devices can be used
+ * with a user space program.
+ *
+ * The device mydriver stores data that you write into.
+ * When you read from this device, previously stored data is returned,
+ * and the internal buffer is erased.
+ *
+ *
+ * To test this application, you just need to:
+ *
+ * $ export LD_LIBRARY_PATH=<path of xenomai>/lib
+ * $ insmod mydriver.ko
+ * $ ./mydrivertest
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <rtdm/rtdm.h>
+
+#define DEV_BASIC0	"rtdm0"
+#define DEV_BASIC1	"rtdm1"
+
+
+#define RTTST_RTDM_DEFER_CLOSE_CONTEXT   1
+#define RTTST_RTDM_MAGIC_PRIMARY        0xfefbfefb
+#define RTTST_RTDM_MAGIC_SECONDARY      0xa5b9a5b9
+
+#define RTDM_CLASS_TESTING          6
+#define RTIOC_TYPE_TESTING              RTDM_CLASS_TESTING
+#define RTTST_RTIOC_RTDM_DEFER_CLOSE \
+        _IOW(RTIOC_TYPE_TESTING, 0x40, __u32)
+
+#define RTTST_RTIOC_RTDM_ACTOR_GET_CPU \
+        _IOR(RTIOC_TYPE_TESTING, 0x41, __u32)
+
+#define RTTST_RTIOC_RTDM_PING_PRIMARY \
+        _IOR(RTIOC_TYPE_TESTING, 0x42, __u32)
+
+#define RTTST_RTIOC_RTDM_PING_SECONDARY \
+        _IOR(RTIOC_TYPE_TESTING, 0x43, __u32)
+
+
+void *test_basic(void *arg)
+{
+    int dev[2], ret[2], err;
+    int from_drv;
+    char task_name[16];
+    snprintf(task_name, sizeof(task_name), "basic-%d", getpid());
+    err = pthread_setname_np(pthread_self(), task_name);
+    if (err)
+	printf("pthread_setname_np(basic)\n");
+    
+    while(1)
+    {
+        /* open the device */
+        dev[0] = rt_dev_open(DEV_BASIC0, 0);
+        dev[1] = rt_dev_open(DEV_BASIC1, 0);
+        
+        if (dev[0] < 0 || dev[1] < 0) {
+            printf("ERROR : can't open device %s (%s) or device %s (%s)\n",
+                   DEV_BASIC0, strerror(-dev[0]), 
+                   DEV_BASIC1, strerror(-dev[1]));
+            fflush(stdout);
+            exit(1);
+        }
+        
+        rt_printf("open sucess %d %d \n", dev[0], dev[1]);
+        //getchar();
+        
+        //err = rt_dev_ioctl (dev[0], RTTST_RTIOC_RTDM_PING_PRIMARY, &from_drv);    
+        //rt_printf("from_drv 0x%0x \n", from_drv);
+        
+        //getchar();
+        err = rt_dev_ioctl (dev[0], RTTST_RTIOC_RTDM_PING_SECONDARY, &from_drv);
+        rt_printf("from_drv 0x%0x \n", from_drv);
+        
+        //getchar();
+        //err = rt_dev_ioctl (dev[0], RTTST_RTIOC_RTDM_DEFER_CLOSE, RTTST_RTDM_DEFER_CLOSE_CONTEXT);
+        
+        //getchar();
+        
+        /* close the device */
+        ret[0] = rt_dev_close(dev[0]);
+        ret[1] = rt_dev_close(dev[1]);
+        if (ret[0] < 0 || ret[1] < 0) {
+            printf("ERROR : can't open device %s (%s) or device %s (%s)\n",
+                   DEV_BASIC0, strerror(-ret[0]), 
+                   DEV_BASIC1, strerror(-ret[1]));
+            fflush(stdout);
+            exit(1);
+        }
+        
+        usleep(1000);
+    }
+}
+
+static int thread_test()
+{
+    struct sched_param param;
+    pthread_attr_t attr;
+    pthread_t tid;
+    void *p;
+    int ret;
+    
+    pthread_attr_init(&attr);
+    param.sched_priority = 0;
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+    pthread_attr_setschedpolicy(&attr, SCHED_OTHER);
+    pthread_attr_setschedparam(&attr, &param);
+    pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED);
+    
+    ret = pthread_create(&tid, &attr, test_basic, NULL);
+    ret = pthread_join(tid, &p);
+    
+    return (int)(long)p;
+}
+
+int main(int argc, char *argv)
+{
+    thread_test();
+    //test_basic();
+
+    return 0;
+}
